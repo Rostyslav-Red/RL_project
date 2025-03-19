42f20fc0146e647f9e9cd97fdd7c89a4dd558fc2
Mila <mila.vanrijsbergen@ru.nl>
2025-03-19 23:08:50 +0100

    my attempt at mc, so far.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from gymnasium.utils.env_checker import check_env
 from dynamic_programming_policy import DynamicProgrammingPolicy
 from policy import Policy
 from temporal_difference_policy import TemporalDifferencePolicy
+from monte_carlo_policy import MonteCarloPolicy
 
 if __name__ == "__main__":
     gym.register(id="Board-v0", entry_point="board:ConfiguredBoard")
@@ -19,12 +20,13 @@ if __name__ == "__main__":
     ### Block for computing policy, only run when computing a new policy
     # p = TemporalDifferencePolicy(board.observation_space, board.action_space).sarsa(board, n_episodes=1000)
     # p.save("policies/td_sarsa.json")
+    p = MonteCarloPolicy(board).policy_evaluation(board)
     ###
 
-    obs, _ = board.reset(options=options, seed=100)
+    #obs, _ = board.reset(options=options, seed=100)
 
-    p = Policy.load(board, "policies/td_sarsa.json")
+    #p = Policy.load(board, "policies/td_sarsa.json")
     # Possible agents: HumanAgent, RandomAgent, PolicyAgent
-    agent = PolicyAgent(board, p)
+    #agent = PolicyAgent(board, p)
 
-    print(f"Obtained reward: {agent.run_agent(obs)}")
+    #print(f"Obtained reward: {agent.run_agent(obs)}")
---
+++ monte_carlo_policy.py
@@ -0,0 +1,86 @@
+from policy import Policy
+import gymnasium as gym
+from typing import List, Tuple
+import numpy as np
+
+
+class MonteCarloPolicy(Policy):
+
+    def __init__(self, environment: gym.Env, policy: dict = None):
+        super().__init__(environment.observation_space, environment.action_space, policy=policy)
+        self.environment = environment
+
+    def first_visit(self, episode: List[Tuple[Tuple[gym.Space, int], float]]):
+        all_v = {}
+        all_q = {}
+        reward = 0
+        episode.reverse()
+        for i in range(len(episode)):
+            reward = 0.1*reward+episode[i][1]
+            if episode[i][0][0] not in list(all_v.keys()):
+                all_v[episode[i][0][0]] = reward
+            if episode[i][0] not in list(all_q.keys()):
+                all_q[episode[i][0]] = reward
+
+        return all_v, all_q
+
+    def policy_evaluation(self, env: gym.Env):
+        policy = Policy(self._obs_space, self._act_space)
+
+        # should call all posible states as an env so i can call the posistions of the cat
+        # and mouse so i can control the reset and it isn't random
+        states = self.keys() # these are tuples
+
+        all_v = {}
+        all_q = {}
+        # runs episodes for each posible action for each posible state
+        for state in states:
+            # print("state")
+            for action in self._all_actions:
+                # print("action")
+                obs = {"cat_position": state.cat_position, "target_position": state.target_position} # doesn't work on tuple
+                env.reset(options=obs)
+                episode = []
+                while True:
+                    new_obs, reward, terminal, truncated, _ = env.step(action)
+                    new_obs = self._obs_to_tuple(new_obs)
+
+                    if terminal or truncated:
+                        break
+
+                    new_action = policy[new_obs]
+
+                    # Update episode
+                    episode.append(((obs, action), float(reward)))
+
+                    action, obs = new_action, new_obs
+
+                # does first visit for bout v and q on an episode
+                episode_v, episode_q = self.first_visit(episode)
+
+                # adds all values of one episode to a big dictionary
+                for k, v in episode_v.items():
+                    if k in all_v.keys():
+                        all_v[k].append(v)
+                    else:
+                        all_v[k] = [v]
+                for k, v in episode_q.items():
+                    if k in all_q.keys():
+                        all_q[k].append(v)
+                    else:
+                        all_q[k] = [v]
+
+        # gets the average for each value in a dictionary getting the proper reward for each v and q
+        avr_v = {}
+        avr_q = {}
+        for k, v in all_v.items():
+            avr_v[k] = np.mean(v)
+        for k, v in all_q.items():
+            avr_q[k] = np.mean(v)
+
+        print(avr_v[list(avr_v.keys())[10]])
+
+
+
+
+
